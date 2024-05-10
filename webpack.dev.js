0a41d73ecb42826241346d40b97e0f9e30755ec1
toduyemi <temit96@hotmail.com>
2024-05-10 17:08:42 -0400

    Change type to modules in package.json

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,12 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 8080,
-        static: './src',
-        hot: true,
-        liveReload: false,
-
-    },
-})
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+    static: './src',
+    hot: true,
+    liveReload: false,
+  },
+});
